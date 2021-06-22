98183c8f67907cf5d98fd1b61d991d45adca67bc
Justasban <justas.ban2001@gmail.com>
2021-06-23 01:56:42 +0300

    exercise 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -21,6 +21,7 @@ const Part = (props) => {
 const Content = props => {
 
   return(
+
     <div>
       <Part content = {props.parts[0].name} number = {props.parts[0].exercises} />
       <Part content = {props.parts[1].name} number = {props.parts[1].exercises} />
@@ -40,33 +41,31 @@ const Total = (props) => {
 
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const parts = [
-
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-
-    {
-    name: 'Using props to pass data',
-    exercises: 7
-    },
-    
-    {
-    name: 'State of a component',
-    exercises: 14
-    }
-
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header courseName={course} />
+      <Header courseName={course.name} />
       
-      <Content parts={parts} />
+      <Content parts={course.parts} />
     
-      <Total parts={parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
