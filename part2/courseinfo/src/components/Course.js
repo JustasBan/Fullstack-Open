dfa0ae05fda043991667fecb78a09a8593612b1a
Justasban <justas.ban2001@gmail.com>
2021-08-08 01:06:55 +0300

    2.1 - 2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Course = ({courses}) => {
+
+  return (
+
+    <div>
+
+      {courses.map((course) => (
+
+        <div key={course.id}>
+
+        <h1 >{course.name}</h1>
+        <div >{course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}</div>
+        <strong >{course.parts.reduce((sum, part) => sum + part.exercises, 0)}</strong>
+        
+        </div>)
+        
+        )}
+
+    </div>
+  )
+}
+
+export default Course
