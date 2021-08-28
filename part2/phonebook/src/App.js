f4329f2ea091d815f604a1b9d638cadda80ec7fc
Justasban <justas.ban2001@gmail.com>
2021-08-29 00:18:36 +0300

    2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import serverComms from './services/serverComms'
 
+const Notification = ({ message, classString }) => {
+
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className={classString}>
+      {message}
+    </div>
+  )
+}
+
 const ListItem = (props) => {
   return (
     <div>
     <p> {props.id} {props.name} {props.number}</p>
-    <button onClick={() => props.deleteHandle(props.id)}>delete</button>
+    <button onClick={() => props.deleteHandle(props.id, props.setNotificationMessage, props.setNotificationMode)}>delete</button>
     </div>
   )
 }
@@ -31,23 +44,47 @@ const PersonForm = (props) => {
       found=true
     }
 
-    if(found){
+    let id = props.persons.filter(person => person.name === personObj.name).map(thing => thing.id)[0]
+    let tempPerson = props.persons.filter(person => person.id === id)
 
-      let id = props.persons.filter(person => person.name === personObj.name).map(thing => thing.id)[0]
-      let tempPerson = props.persons.filter(person => person.id === id)
+    if(found){
 
       if(window.confirm(tempPerson.map(thing => thing.name) + ' is already added to phonebook, replace number?')){
       
-      const changedPerson =  { ...tempPerson[0], number: props.newNumber}
+        const changedPerson =  { ...tempPerson[0], number: props.newNumber}
 
-      serverComms
-        .change(changedPerson)
-        .then(response =>{
+        serverComms
+          .change(changedPerson)
+          .then(response =>{
 
-          console.log('change promise fullfiled');
+            console.log('change promise fullfiled');
 
-          props.setPersons(props.persons.map(person => person.id !== id ? person : response.data))
-        })
+            props.setNotificationMode('change')
+
+            props.setNotificationMessage(
+              `Person '${tempPerson.map(thing => thing.name)}' number changed`
+            )
+
+            setTimeout(()=> {
+              props.setNotificationMessage(null)
+            }, 5000)
+
+            props.setPersons(props.persons.map(person => person.id !== id ? person : response.data))
+          })
+          .catch(error => {
+            
+            props.setNotificationMode('error')
+
+            props.setNotificationMessage(
+              `Person '${tempPerson.map(thing => thing.name)}' already removed`
+            )
+
+            setTimeout(()=> {
+              props.setNotificationMessage(null)
+            }, 5000)
+
+            props.setPersons(props.persons.filter(n => n.id !== id))
+          })
       }
     }
     else{
@@ -58,6 +95,16 @@ const PersonForm = (props) => {
 
           console.log('adding promise fullfiled');
           props.setPersons(props.persons.concat(initialPerson))
+
+          props.setNotificationMode('change')
+
+          props.setNotificationMessage(
+            `'${initialPerson.name}' added`
+          )
+
+          setTimeout(()=> {
+            props.setNotificationMessage(null)
+          }, 5000)
 
           props.setNewName('')
           props.setNewNumber('')
@@ -91,7 +138,7 @@ const Persons = (props) => {
   return(
     <div>
       <h2>Numbers</h2>
-      {props.personsToShow.map(person => <ListItem key={person.name} name={person.name} number={person.number} id={person.id} deleteHandle={props.deleteHandle}/>)}
+      {props.personsToShow.map(person => <ListItem key={person.name} name={person.name} number={person.number} id={person.id} deleteHandle={props.deleteHandle} setNotificationMessage={props.setNotificationMessage} setNotificationMode={props.setNotificationMode}/>)}
     </div>
   )
 }
@@ -104,6 +151,8 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+  const [ notificationMessage, setNotificationMessage ] = useState(null)
+  const [ notificationMode, setNotificationMode ] = useState('')
 
   useEffect(() => {
     serverComms
@@ -132,7 +181,7 @@ const App = () => {
 
   }
 
-  const deleteHandle = id =>{
+  const deleteHandle = (id, setNotificationMessage, setNotificationMode) =>{
 
     let tempPerson = persons.filter(person => person.id === id)
 
@@ -143,6 +192,17 @@ const App = () => {
         .erase(id)
         .then(response => {
           console.log('delete promise fullfilled');
+
+          setNotificationMode('change')
+
+          setNotificationMessage(
+            `${tempPerson.map(thing => thing.name)} deleted`
+          )
+
+          setTimeout(()=> {
+            setNotificationMessage(null)
+          }, 5000)
+
           setPersons(persons.filter(person => person.id !== id))
         })
     }
@@ -171,10 +231,10 @@ const App = () => {
 
   return (
     <div>
-
+      <Notification message={notificationMessage} classString={notificationMode}/>
     <Search newFilter={newFilter} handleFilterChange={handleFilterChange}/>
-    <PersonForm replaceNum={replaceNum} setPersons={setPersons} persons={persons} setNewName={setNewName} setNewNumber={setNewNumber} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
-    <Persons personsToShow={persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))} deleteHandle={deleteHandle}/> 
+    <PersonForm setNotificationMessage={setNotificationMessage} setNotificationMode={setNotificationMode} replaceNum={replaceNum} setPersons={setPersons} persons={persons} setNewName={setNewName} setNewNumber={setNewNumber} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+    <Persons personsToShow={persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))} deleteHandle={deleteHandle} setNotificationMessage={setNotificationMessage} setNotificationMode={setNotificationMode}/> 
 
     </div>
   )
